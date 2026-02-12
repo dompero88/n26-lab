6359e694f654395de3af67c2bc64927ccca6ca3b
domhsr03 <asgarozzo90@gmail.com>
2026-02-12 18:43:03 +0100

    fix: use string for platform to avoid AttributeError

--- infrastructure/infra/infra_stack.py
+++ infrastructure/infra/infra_stack.py
@@ -10,33 +10,26 @@ class InfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # 1. Recupera la VPC di default
         vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
-
-        # 2. Definisci il Cluster ECS
         cluster = ecs.Cluster(self, "N26Cluster", vpc=vpc)
 
-        # 3. Definisci il Servizio Fargate (Load Balancer + Container)
         self.service = ecs_patterns.ApplicationLoadBalancedFargateService(
             self, "N26AiService",
             cluster=cluster,
-            cpu=256,            # 0.25 vCPU
-            memory_limit_mib=512, # 0.5 GB RAM
+            cpu=256,
+            memory_limit_mib=512,
             desired_count=1,
-            # FORZA IL RUNTIME: Dice ad AWS di usare macchine Intel/AMD64
             runtime_platform=ecs.RuntimePlatform(
                 operating_system_family=ecs.OperatingSystemFamily.LINUX,
                 cpu_architecture=ecs.CpuArchitecture.X86_64
             ),
             task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-                # FORZA IL BUILD: Dice a Docker di buildare in formato AMD64
                 image=ecs.ContainerImage.from_asset(
                     "../app",
-                    platform=ecs.AssetPlatform.LINUX_AMD64
+                    platform="linux/amd64" # Forza build Intel/AMD64
                 ),
                 container_port=80,
                 environment={
-                    # IP PRIVATO dell'istanza EC2 dove gira Ollama
                     "OLLAMA_ENDPOINT": "http://172.31.23.1:11434" 
                 }
             ),
@@ -44,7 +37,6 @@ class InfraStack(Stack):
             assign_public_ip=True
         )
 
-        # Opzionale: Health Check personalizzato per dare tempo all'app di avviarsi
         self.service.target_group.configure_health_check(
             path="/health",
             interval=30,
